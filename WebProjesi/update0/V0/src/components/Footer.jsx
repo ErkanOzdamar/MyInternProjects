927ee41a0073ba4ece760e74699efcb170820622
Erkan Özdamar <158081686+ErkanOzdamar@users.noreply.github.com>
2025-10-22 23:26:46 +0300

    Delete WebProjesi directory

--- WebProjesi/update0/V0/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./Footer.css";
-export default function Footer() {
-  return (
-     <div className="footer">
-      <h2>Ben bir footer bileşeniyim!</h2>
-      <p>Buraya kısa bir açıklama yazabilirsin.</p>
-    </div>
-  );
-}
